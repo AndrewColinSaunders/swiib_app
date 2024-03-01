fe3aa43d6bd60b6450e5c2b961a1d92e20a8120b
Andrew <andrewsaunders410@gmail.com>
2024-03-01 13:57:32 +0200

    Update ProductsActivity with new functionality NEW

--- app/src/main/java/com/example/warehousetet/ProductsActivity.kt
+++ app/src/main/java/com/example/warehousetet/ProductsActivity.kt
@@ -1,73 +1,3 @@
-//package com.example.warehousetet
-//
-//import android.os.Bundle
-//import android.util.Log
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import androidx.recyclerview.widget.RecyclerView
-//import kotlinx.coroutines.CoroutineScope
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.cancel
-//import kotlinx.coroutines.launch
-//import kotlinx.coroutines.withContext
-//
-//class ProductsActivity : AppCompatActivity() {
-//    private lateinit var productsAdapter: ProductsAdapter
-//    private val coroutineScope = CoroutineScope(Dispatchers.IO)
-//    private lateinit var odooXmlRpcClient: OdooXmlRpcClient
-//
-//    private var productBarcodes = hashMapOf<String, String>() // Map product names to their barcodes
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_products)
-//
-//        odooXmlRpcClient = OdooXmlRpcClient(CredentialManager(this))
-//
-//        val receiptId = intent.getIntExtra("RECEIPT_ID", -1)
-//        if (receiptId != -1) {
-//            setupRecyclerView()
-//            fetchProductsForReceipt(receiptId)
-//        } else {
-//            Log.e("ProductsActivity", "Invalid receipt ID passed to ProductsActivity")
-//        }
-//        fetchBarcodesForProducts()
-//    }
-//
-//    private fun setupRecyclerView() {
-//        val recyclerView: RecyclerView = findViewById(R.id.productsRecyclerView)
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//        productsAdapter = ProductsAdapter(emptyList())
-//        recyclerView.adapter = productsAdapter
-//    }
-//
-//    private fun fetchProductsForReceipt(receiptId: Int) {
-//        coroutineScope.launch {
-//            val products = odooXmlRpcClient.fetchProductsForReceipt(receiptId)
-//            withContext(Dispatchers.Main) {
-//                productsAdapter.updateProducts(products)
-//            }
-//        }
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        coroutineScope.cancel()
-//    }
-//
-//    private fun fetchBarcodesForProducts() {
-//        productsAdapter.products.forEach { product ->
-//            coroutineScope.launch {
-//                val barcode = odooXmlRpcClient.fetchProductBarcodeByName(product.name)
-//                if (barcode != null) {
-//                    productBarcodes[product.name] = barcode
-//                }
-//            }
-//        }
-//    }
-//}
-
-
 package com.example.warehousetet
 
 import android.content.Context
@@ -149,15 +79,6 @@ class ProductsActivity : AppCompatActivity() {
         recyclerView.adapter = productsAdapter
     }
 
-//    private fun fetchProductsForReceipt(receiptId: Int) {
-//        coroutineScope.launch {
-//            val products = odooXmlRpcClient.fetchProductsForReceipt(receiptId)
-//            withContext(Dispatchers.Main) {
-//                productsAdapter.updateProducts(products)
-//                fetchBarcodesForProducts(products) // Fetch barcodes after products are updated
-//            }
-//        }
-//    }
 private fun fetchProductsForReceipt(receiptId: Int) {
     coroutineScope.launch {
         // Fetch products for the receipt
