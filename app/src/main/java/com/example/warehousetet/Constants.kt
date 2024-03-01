806e3e0be55d214abd3bf0197958352ef4c080fb
Andrew <andrewsaunders410@gmail.com>
2024-03-01 10:10:57 +0200

    Remove api key

--- app/src/main/java/com/example/warehousetet/Constants.kt
+++ app/src/main/java/com/example/warehousetet/Constants.kt
@@ -2,7 +2,7 @@ package com.example.warehousetet
 
 object Constants {
 
-    const val API_KEY = "1fd376690d1910f9ad917ce0e095e6038e322f87"
+
     const val DATABASE = "db2"
     const val URL = "https://db2.swiib.app/"
 
